9b8849c49ff3dc4387dbf35ed13ea7ff1b95e517
Vikas Sonwal <vikassonwal@gmail.com>
2017-05-02 16:14:32 +0530

    updated user change functionality

---
+++ demo.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    var editor;
+
+    var users = [
+        { id: 1, name: "Tarani" },
+        { id: 2, name: "Kapil" },
+        { id: 3, name: "Pushpendra" },
+        { id: 4, name: "Vikas" }
+    ];
+    
+    loadEditor('editor1', true);
+    
+    var select = $('#username')[0];
+    $.each(users, function(i, user) {
+        var option = new Option(user.name, user.id);
+        select.options[i] = option;
+    });
+    $('#username').change(onUserChanged);
+
+    function onUserChanged(event) {
+        var target = event.currentTarget;
+        var id = $(target).val();
+        selectUser(id);
+        editor.focus();
+    }
+
+    function selectUser(id) {
+        var i;
+        for (i = 0; i < users.length; ++i) {
+            if (users[i].id == id) {
+                break;
+            }
+        }
+        var user = users[i];
+        if (user && editor) {
+			var lite = editor.plugins.lite;
+			lite && lite.findPlugin(editor).setUserInfo(user);
+        }
+    }
+
+
+    function loadEditor(id, focus) {
+        editor = CKEDITOR.replace(id);
+
+        function onConfigLoaded(e) {
+            var conf = e.editor.config;
+            var lt = conf.lite = conf.lite || {};
+            if (location.href.indexOf("debug") > 0) {
+                lt.includeType = "debug";
+            }
+        }
+
+        editor.on('configLoaded', onConfigLoaded);
+    }
+});
--- doc-editor/config.js
+++ doc-editor/config.js
@@ -3,9 +3,37 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-	config.extraPlugins = 'lite';
+CKEDITOR.editorConfig = function(config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    var lite = config.lite = config.lite || {};
+    config.extraPlugins = 'lite';
+
+    lite.includes_debug = ["js/rangy/rangy-core.js", "js/ice.js", "js/dom.js", "js/selection.js", "js/bookmark.js", "lite-interface.js"];
+    lite.includes = ["lite-includes.min.js"];
+    lite.includes_full = ["lite-includes.js"];
+    // set to false if you want change tracking to be off initially
+    lite.isTracking = true;
+    lite.userStyles = {
+        "21": 3,
+        "15": 1,
+        "18": 2
+    };
+
+    // these are the default tooltip values. If you want to use this default configuration, just set lite.tooltips = true;
+    lite.tooltips = {
+        show: true,
+        path: "js/opentip-adapter.min.js",
+        classPath: "OpentipAdapter",
+        cssPath: "css/opentip.css",
+        delay: 500
+    };
+    lite.tooltipTemplate = "%a by %u, first edit %t, last edit %T";
+    //	lite.commands = [/*LITE.Commands.TOGGLE_TRACKING, */LITE.Commands.TOGGLE_SHOW/*, LITE.Commands.ACCEPT_ALL, LITE.Commands.REJECT_ALL, LITE.Commands.ACCEPT_ONE, LITE.Commands.REJECT_ONE */];
+    config.enterMode = CKEDITOR.ENTER_BR;
+    config.autoParagraph = false;
+    config.title = false;
+
+
 };
